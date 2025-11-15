92a171c323091cfbf4be97e4ab7e9e6644ea5762
Nikhil <nikhilkumarnani@gmail.com>
2025-11-15 06:37:58 +0000

    what about firebase are they hardcoded?

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,13 @@ const nextConfig: NextConfig = {
     allowedDevOrigins: [
       "https://6000-firebase-studio-1763112901981.cluster-c36dgv2kibakqwbbbsgmia3fny.cloudworkstations.dev"
     ]
+  },
+  env: {
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   }
 };
 
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-5838952157-e53ed",
-  "appId": "1:320735012084:web:22abe744fff86e09afaaa1",
-  "apiKey": "AIzaSyAIEDtC8hAagCUJrgtWdec7jm-sfm34jJc",
-  "authDomain": "studio-5838952157-e53ed.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "320735012084"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
